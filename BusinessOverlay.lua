b6f2a9679f7b7ed0e0012e252a45c5768598619f
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-04-30 18:04:55 -0700

    refactor: improve lookup

--- BusinessOverlay.lua
+++ BusinessOverlay.lua
@@ -3,7 +3,7 @@ local directx, util = directx, util
 
 local og_stat_get_int64 = util.stat_get_int64
 util.stat_get_int64 = function(stat)
-    return og_stat_get_int64('MP' .. util.get_char_slot() .. '_' .. stat)
+	return og_stat_get_int64('MP' .. util.get_char_slot() .. '_' .. stat)
 end
 
 local draw = true
@@ -22,52 +22,52 @@ local gap_2 = 0.16
 local row_gap = 0.0165
 local text_size = 0.425
 
-local views = {
-	{ label = 'Nightclub', state = true },
-	{ label = 'Arcade', state = true },
-	{ label = 'Agency', state = true },
-	{ label = 'Cash', state = true },
-	{ label = 'Forgery', state = true },
-	{ label = 'Weed', state = true },
-	{ label = 'Cocaine', state = true },
-	{ label = 'Meth', state = true },
-	{ label = 'Bunker', state = true },
-	{ label = 'Acid Lab', state = true },
-	{ label = 'Hub Cargo', state = true },
-	{ label = 'Hub Weapons', state = true },
-	{ label = 'Hub Cocaine', state = true },
-	{ label = 'Hub Meth', state = true },
-	{ label = 'Hub Forgery', state = true },
-	{ label = 'Hub Weed', state = true },
-	{ label = 'Hub Cash', state = true }
-}
-local data = {
-	{ max = 250000 }, -- nightclub
-	{ max = 100000 }, -- arcade safe
-	{ max = 250000 }, -- agency safe
-	{ delim = '/', max = 40 }, -- cash
-	{ delim = '/', max = 60 }, -- forgery
-	{ delim = '/', max = 80 }, -- weed
-	{ delim = '/', max = 10 }, -- cocaine
-	{ delim = '/', max = 20 }, -- meth
-	{ delim = '/', max = 100 }, -- bunker
-	{ delim = '/', max = 160 }, -- acid lab
-	{ delim = '/', max = 50 }, -- hub cargo
-	{ delim = '/', max = 100 }, -- hub weapons
-	{ delim = '/', max = 10 }, -- hub cocaine
-	{ delim = '/', max = 20 }, -- hub meth
-	{ delim = '/', max = 60 }, -- hub forgery
-	{ delim = '/', max = 80 }, -- hub weed
-	{ delim = '/', max = 40 } -- hub cash
-}
-
 local background_colour = { r = 0, g = 0, b = 0, a = 0.75 }
 local text_colour = { r = 1, g = 1, b = 1, a = 1 }
 local max_colour = { r = 0, g = 1, b = 0, a = 1 }
 
+local views = {
+	{ label = 'Nightclub',   state = true },
+	{ label = 'Arcade',      state = true },
+	{ label = 'Agency',      state = true },
+	{ label = 'Cash',        state = true },
+	{ label = 'Forgery',     state = true },
+	{ label = 'Weed',        state = true },
+	{ label = 'Cocaine',     state = true },
+	{ label = 'Meth',        state = true },
+	{ label = 'Bunker',      state = true },
+	{ label = 'Acid Lab',    state = true },
+	{ label = 'Hub Cargo',   state = true },
+	{ label = 'Hub Weapons', state = true },
+	{ label = 'Hub Cocaine', state = true },
+	{ label = 'Hub Meth',    state = true },
+	{ label = 'Hub Forgery', state = true },
+	{ label = 'Hub Weed',    state = true },
+	{ label = 'Hub Cash',    state = true }
+}
+local data = {
+	{ max = 250000, stat_1 = 'CLUB_POPULARITY', stat_2 = 'CLUB_SAFE_CASH_VALUE' }, -- nightclub
+	{ max = 100000, stat_2 = 'ARCADE_SAFE_CASH_VALUE' }, -- arcade
+	{ max = 250000, stat_2 = 'FIXER_SAFE_CASH_VALUE' }, -- agency safe
+	{ delim = '/',  max = 40, stat_1 = 'MATTOTALFORFACTORY0', stat_2 = 'PRODTOTALFORFACTORY0' }, -- cash
+	{ delim = '/',  max = 60, stat_1 = 'MATTOTALFORFACTORY4', stat_2 = 'PRODTOTALFORFACTORY4' }, -- forgery
+	{ delim = '/',  max = 80, stat_1 = 'MATTOTALFORFACTORY3', stat_2 = 'PRODTOTALFORFACTORY3' }, -- weed
+	{ delim = '/',  max = 10, stat_1 = 'MATTOTALFORFACTORY1', stat_2 = 'PRODTOTALFORFACTORY1' }, -- cocaine
+	{ delim = '/',  max = 20, stat_1 = 'MATTOTALFORFACTORY2', stat_2 = 'PRODTOTALFORFACTORY2' }, -- meth
+	{ delim = '/',  max = 100, stat_1 = 'MATTOTALFORFACTORY5', stat_2 = 'PRODTOTALFORFACTORY5' }, -- bunker
+	{ delim = '/',  max = 160, stat_1 = 'MATTOTALFORFACTORY6', stat_2 = 'PRODTOTALFORFACTORY6' }, -- acid lab
+	{ delim = '/',  max = 50, stat_2 = 'HUB_PROD_TOTAL_0' }, -- hub cargo
+	{ delim = '/',  max = 100, stat_2 = 'HUB_PROD_TOTAL_1' }, -- hub weapons
+	{ delim = '/',  max = 10, stat_2 = 'HUB_PROD_TOTAL_2' }, -- hub cocaine
+	{ delim = '/',  max = 20, stat_2 = 'HUB_PROD_TOTAL_3' }, -- hub meth
+	{ delim = '/',  max = 60, stat_2 = 'HUB_PROD_TOTAL_5' }, -- hub forgery
+	{ delim = '/',  max = 80, stat_2 = 'HUB_PROD_TOTAL_4' }, -- hub weed
+	{ delim = '/',  max = 40, stat_2 = 'HUB_PROD_TOTAL_6' } -- hub cash
+}
+
 root:toggle('Enabled', {}, '', function(s) draw = s end, draw)
 
-local bools = root:list('Views', {}, 'Configure the businesses to monitor.')
+local bools = root:list('Views', {}, 'Configure the views to monitor.')
 for _, v in views do
 	bools:toggle(v.label, {}, '', function(s)
 		v.state = s
@@ -75,7 +75,7 @@ for _, v in views do
 end
 
 local children = bools:getChildren()
-local ref = bools:list_select('State', {}, '', {'Enable All', 'Disable All' }, 1, function(idx)
+local ref = bools:list_select('State', {}, '', { 'Enable All', 'Disable All' }, 1, function(idx)
 	for _, v in children do
 		v.value = idx == 1
 	end
@@ -83,198 +83,47 @@ end):detach()
 children[1]:attachBefore(ref)
 
 root:divider('Configuration')
-root:slider_float('Width', {}, '', 0, 300, 17, 1, function(v) 
-	width = v / 100 
+root:slider_float('Width', {}, '', 0, 300, 17, 1, function(v)
+	width = v / 100
 end)
-root:slider_float('Max Height', {}, 'A scalar used to formulaiclly to determine the height of the window; does not truly determine the height of the window.', 0, 1000, 26, 1, function(v)
-	max_height = v / 100
+root:slider_float('Max Height', {},
+	'A scalar used to formulaiclly determine the height of the window given the number of lines; does not truly represent the window height.',
+	0, 1000, 26, 1, function(v)
+		max_height = v / 100
+	end)
+root:slider_float('Max Height Enforced', {},
+	'The true value of the window height to clamp to when the calculated height is larger.', 0, 1000, 31, 1, function(v)
+		max_height_alt = v / 100
+	end)
+root:slider_float('X Position', {}, '', 0, 83, 67, 1, function(v)
+	x = v / 100
 end)
-root:slider_float('Max Height Enforced', {}, 'The true value of the window height to clamp to when the calculated height is larger.', 0, 1000, 31, 1, function(v)
-	max_height_alt = v / 100
+root:slider_float('Y Position', {}, '', 0, 71, 0, 1, function(v)
+	y = v / 100
 end)
-root:slider_float('X Position', {}, '', 0, 83, 67, 1, function(v) 
-	x = v / 100 
-end)
-root:slider_float('Y Position', {}, '', 0, 71, 0, 1, function(v) 
-	y = v / 100 
-end)
-root:slider_float('Left Column Offset', {}, 'The offset of the left column (supplies) from the origin (the default value.)', -66, 32, 11, 1, function(v)
-	gap_1 = v / 100 
-end)
-root:slider_float('Right Column Offset', {}, 'The offset of the right column (product) from the origin (the default value.)', -68, 32, 16, 1, function(v) 
-	gap_2 = v / 100 
-end)
-root:slider_float('Row Gap', {}, '', 0, 1000, 165, 1, function(v) 
+root:slider_float('Left Column Offset', {},
+	'The offset of the left column (supplies) from the origin (the default value.)', -66, 32, 11, 1, function(v)
+		gap_1 = v / 100
+	end)
+root:slider_float('Right Column Offset', {},
+	'The offset of the right column (product) from the origin (the default value.)', -68, 32, 16, 1, function(v)
+		gap_2 = v / 100
+	end)
+root:slider_float('Row Gap', {}, '', 0, 1000, 165, 1, function(v)
 	row_gap = v / 10000
 end)
-root:slider_float('Text Size', {}, '', 0, 1000, 425, 1, function(v) 
-	text_size = v / 1000 
+root:slider_float('Text Size', {}, '', 0, 1000, 425, 1, function(v)
+	text_size = v / 1000
 end)
 root:colour('Background Colour', {}, '', background_colour, true, function(c)
 	background_colour = c
 end)
-root:colour('Text Colour', {}, '', text_colour, false, function(c) 
+root:colour('Text Colour', {}, '', text_colour, false, function(c)
 	text_colour = c
 end)
 root:colour('Max Colour', {}, '', max_colour, false, function(c)
 	max_colour = c
 end)
-
-local function populate()
-	for i = 1, #views do
-		if not views[i].state then
-			goto continue
-		end
-
-		local c = views[i]
-		switch i do
-			-- nightclub
-			case 1:
-				c.value_1 = tostring(
-					util.stat_get_int64('CLUB_POPULARITY') / 10
-				):gsub('%.?0+$', '') .. '%'
-				c.value_2 = {
-					val = util.stat_get_int64('CLUB_SAFE_CASH_VALUE'),
-					max = 250000
-				}
-			break
-			-- arcade safe
-			case 2: 
-				c.value_2 = {
-					val = util.stat_get_int64('ARCADE_SAFE_CASH_VALUE'),
-					max = 100000
-				}
-			break
-			-- agency safe
-			case 3:
-				c.value_2 = {
-					val = util.stat_get_int64('FIXER_SAFE_CASH_VALUE'),
-					max = 250000
-				}
-			break
-			-- cash
-			case 4:
-				c.value_1 = util.stat_get_int64('MATTOTALFORFACTORY' .. 0) .. '%'
-				c.value_2 = {
-					val = util.stat_get_int64('PRODTOTALFORFACTORY' .. 0),
-					delim = '/',
-					max = 40-- get_global_int(18941)
-				}
-			break
-			-- forgery
-			case 5:
-				c.value_1 = util.stat_get_int64('MATTOTALFORFACTORY' .. 4) .. '%'
-				c.value_2 = {
-					val = util.stat_get_int64('PRODTOTALFORFACTORY' .. 4),
-					delim = '/',
-					max = 60--get_global_int(18933)
-				}
-			break
-			-- weed
-			case 6:
-				c.value_1 = util.stat_get_int64('MATTOTALFORFACTORY' .. 3) .. '%'
-				c.value_2 = {
-					val = util.stat_get_int64('PRODTOTALFORFACTORY' .. 3),
-					delim = '/',
-					max = 80--get_global_int(18909)
-				}
-			break
-			-- cocaine
-			case 7:
-				c.value_1 = util.stat_get_int64('MATTOTALFORFACTORY' .. 1) .. '%'
-				c.value_2 = {
-					val = util.stat_get_int64('PRODTOTALFORFACTORY' .. 1),
-					delim = '/',
-					max = 10--get_global_int(18925)
-				}
-			break
-			-- meth
-			case 8:
-				c.value_1 = util.stat_get_int64('MATTOTALFORFACTORY' .. 2) .. '%'
-				c.value_2 = {
-					val = util.stat_get_int64('PRODTOTALFORFACTORY' .. 2),
-					delim = '/',
-					max = 20--get_global_int(18917)
-				}
-			break
-			-- bunker
-			case 9:
-				c.value_1 = util.stat_get_int64('MATTOTALFORFACTORY' .. 5) .. '%'
-				c.value_2 = {
-					val = util.stat_get_int64('PRODTOTALFORFACTORY' .. 5),
-					delim = '/',
-					max = 100--get_global_int(21531)
-				}
-			break
-			-- acid lab
-			case 10:
-				c.value_1 = util.stat_get_int64('MATTOTALFORFACTORY' .. 6) .. '%'
-				c.value_2 = {
-					val = util.stat_get_int64('PRODTOTALFORFACTORY' .. 6),
-					delim = '/',
-					max = 160--get_global_int(18949),
-				}
-			break
-			-- hub cargo
-			case 11:
-				c.value_2 = {
-					val = util.stat_get_int64('HUB_PROD_TOTAL_' .. 0),
-					delim = '/',
-					max = 50--get_global_int(24394)
-				}
-			break
-			-- hub weapons
-			case 12:
-				c.value_2 = {
-					val = util.stat_get_int64('HUB_PROD_TOTAL_' .. 1),
-					delim = '/',
-					max = 100--get_global_int(24388)
-				}
-			break
-			-- hub cocaine
-			case 13:
-				c.value_2 = {
-					val = util.stat_get_int64('HUB_PROD_TOTAL_' .. 2),
-					delim = '/',
-					max = 10--get_global_int(24389)
-				}
-			break
-			-- hub meth
-			case 14:
-				c.value_2 = {
-					val = util.stat_get_int64('HUB_PROD_TOTAL_' .. 3),
-					delim = '/',
-					max = 20--get_global_int(24390)
-				}
-			break
-			-- hub forgery
-			case 15:
-				c.value_2 = {
-				    val = util.stat_get_int64('HUB_PROD_TOTAL_' .. 5),
-				    delim = '/', 
-				    max = 60--get_global_int(24392)
-				}
-			break
-			-- hub weed
-			case 16:
-				c.value_2 = {
-				    val = util.stat_get_int64('HUB_PROD_TOTAL_' .. 4),
-				    delim = '/',
-				    max = 80--get_global_int(24391)
-				}
-			break
-			-- hub cash
-			case 17:
-				c.value_2 = {
-					val = util.stat_get_int64('HUB_PROD_TOTAL_' .. 6),
-					delim = '/',
-					max = 40--get_global_int(24393)
-				}
-			break
-		end
-		::continue::
-	end
-end
 
 local function get_line_count()
 	local c = 0
@@ -296,10 +145,8 @@ util.create_tick_handler(function()
 		--util.draw_centred_text('overlay: waiting for session transition')
 		util.yield()
 	end
-	
-	if draw then
-		populate()
 
+	if draw then
 		local last_pos = y
 		local height = calculate_height(get_line_count())
 		if height > max_height_alt then
@@ -315,18 +162,27 @@ util.create_tick_handler(function()
 
 			last_pos = last_pos + row_gap
 			directx.draw_text(x + 0.003, last_pos, v.label, ALIGN_TOP_LEFT, text_size, text_colour)
-			
-			if v.value_1 then
-				directx.draw_text(x + gap_1, last_pos, v.value_1, ALIGN_TOP_RIGHT, text_size, text_colour)
+
+			local curr = data[k]
+			local stat_1, stat_2 = curr.stat_1, curr.stat_2
+
+			if stat_1 then
+				directx.draw_text(
+					x + gap_1, last_pos, math.floor(util.stat_get_int64(stat_1)) .. '%', ALIGN_TOP_RIGHT,
+					text_size,
+					text_colour
+				)
 			end
 
-			local str = v.value_2.val
+			local val = util.stat_get_int64(stat_2)
+			local str = val
 			local colour = text_colour
 
-			if v.value_2.delim ~= nil then
-				str = str .. v.value_2.delim .. v.value_2.max
+			if curr.delim ~= nil then
+				str = str .. curr.delim .. curr.max
 			end
-			if v.value_2.val == v.value_2.max then
+
+			if val == curr.max then
 				colour = max_colour
 			end
 
